b8b35751ef8eebad9fc909a431db08d86bba021d
rotoglup <rotoglup@02a940b4-97ce-11de-a20b-5bf1f98922f7>
2010-09-12 17:04:50 +0000

    image resize, fix rescale_any_view compilation on GCC

--- rtgu/image/rescale_any_view.hpp
+++ rtgu/image/rescale_any_view.hpp
@@ -20,28 +20,28 @@ namespace boost { namespace gil {
 
 //----------------------------------------------------------------------------
 
-namespace boost { namespace gil { namespace detail
-{
-  template <typename Views>
-  struct rescale_obj
-  {
-    typedef void result_type;        // required typedef
+namespace boost { namespace gil { 
 
-    any_image_view<Views>& _dst_view;
+namespace detail {
+
+  struct rescale_obj : public binary_operation_obj<rescale_obj>
+  {
     const detail::weight_table& _horizontal_weights;
     const detail::weight_table& _vertical_weights;
 
-    rescale_obj(any_image_view<Views>& dst_view, const detail::weight_table& horizontal_weights, const detail::weight_table& vertical_weights)
-      : _dst_view(dst_view), _horizontal_weights(horizontal_weights), _vertical_weights(vertical_weights)
+    rescale_obj(const detail::weight_table& horizontal_weights, const detail::weight_table& vertical_weights)
+      : _horizontal_weights(horizontal_weights), _vertical_weights(vertical_weights)
     {}
 
-    template <typename SrcView>
-    void operator()(SrcView& src) const
+    template <typename V1, typename V2>
+    GIL_FORCEINLINE bool apply_compatible(const V1& v1, const V2& v2) const
     {
-      return rescale( src, _dst_view._dynamic_cast<SrcView>(), _horizontal_weights, _vertical_weights );
+      return rescale( v1, v2, _horizontal_weights, _vertical_weights );
     }
   };
-} } }
+}
+
+} }
 
 template <typename ViewTypes, typename Filter>
 inline void boost::gil::rescale_any_view(any_image_view<ViewTypes>& src, any_image_view<ViewTypes>& dst, const Filter& filter)
@@ -56,13 +56,13 @@ inline void boost::gil::rescale_any_view(any_image_view<ViewTypes>& src, any_ima
 
   // rescale
 
-  apply_operation( src, detail::rescale_obj<ViewTypes>(dst, horizontal_weights, vertical_weights) );
+  apply_operation( src, dst, detail::rescale_obj(horizontal_weights, vertical_weights) );
 }
 
 template <typename ViewTypes>
 inline void boost::gil::rescale_any_view(any_image_view<ViewTypes>& src, any_image_view<ViewTypes>& dst, const detail::weight_table& horizontal_weights, const detail::weight_table& vertical_weights)
 {
-  apply_operation( src, detail::rescale_obj<ViewTypes>(dst, horizontal_weights, vertical_weights) );
+  apply_operation( src, dst, detail::rescale_obj(horizontal_weights, vertical_weights) );
 }
 
 //----------------------------------------------------------------------------
