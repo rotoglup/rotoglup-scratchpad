340725ac9307faec8d90fc6dbcf2e6fac3ecf0f4
rotoglup@gmail.com <rotoglup@gmail.com@02a940b4-97ce-11de-a20b-5bf1f98922f7>
2011-04-23 21:24:57 +0000

    experiments: python ctypes

--- experiments/python_ctypes_and_c_module/rtgu_matrix.c
+++ experiments/python_ctypes_and_c_module/rtgu_matrix.c
@@ -10,11 +10,43 @@ void rtgu_mat44_identity(rtgu_mat44 m)
   _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
 }
 
-void rtgu_mat44_rotationX(rtgu_mat44 m, float cosx, float sinx)
+void rtgu_mat44_rotation_x(rtgu_mat44 m, float cosx, float sinx)
 {
   _M(0,0) = 1.0f; _M(0,1) = 0.0f; _M(0,2) = 0.0f; _M(0,3) = 0.0f;
   _M(1,0) = 0.0f; _M(1,1) = cosx; _M(1,2) =-sinx; _M(1,3) = 0.0f;
   _M(2,0) = 0.0f; _M(2,1) = sinx; _M(2,2) = cosx; _M(2,3) = 0.0f;
+  _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
+}
+
+void rtgu_mat44_rotation_y(rtgu_mat44 m, float cosx, float sinx)
+{
+  _M(0,0) = cosx; _M(0,1) = 0.0f; _M(0,2) = sinx; _M(0,3) = 0.0f;
+  _M(1,0) = 0.0f; _M(1,1) = 1.0f; _M(1,2) = 0.0f; _M(1,3) = 0.0f;
+  _M(2,0) =-sinx; _M(2,1) = 0.0f; _M(2,2) = cosx; _M(2,3) = 0.0f;
+  _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
+}
+
+void rtgu_mat44_rotation_z(rtgu_mat44 m, float cosx, float sinx)
+{
+  _M(0,0) = cosx; _M(0,1) =-sinx; _M(0,2) = 0.0f; _M(0,3) = 0.0f;
+  _M(1,0) = sinx; _M(1,1) = cosx; _M(1,2) = 0.0f; _M(1,3) = 0.0f;
+  _M(2,0) = 0.0f; _M(2,1) = 0.0f; _M(2,2) = 1.0f; _M(2,3) = 0.0f;
+  _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
+}
+
+void rtgu_mat44_scale(rtgu_mat44 m, float x, float y, float z)
+{
+  _M(0,0) =    x; _M(0,1) = 0.0f; _M(0,2) = 0.0f; _M(0,3) = 0.0f;
+  _M(1,0) = 0.0f; _M(1,1) =    y; _M(1,2) = 0.0f; _M(1,3) = 0.0f;
+  _M(2,0) = 0.0f; _M(2,1) = 0.0f; _M(2,2) =    z; _M(2,3) = 0.0f;
+  _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
+}
+
+void rtgu_mat44_translation(rtgu_mat44 m, float x, float y, float z)
+{
+  _M(0,0) = 1.0f; _M(0,1) = 0.0f; _M(0,2) = 0.0f; _M(0,3) = x;
+  _M(1,0) = 0.0f; _M(1,1) = 1.0f; _M(1,2) = 0.0f; _M(1,3) = y;
+  _M(2,0) = 0.0f; _M(2,1) = 0.0f; _M(2,2) = 1.0f; _M(2,3) = z;
   _M(3,0) = 0.0f; _M(3,1) = 0.0f; _M(3,2) = 0.0f; _M(3,3) = 1.0f;
 }
 
--- experiments/python_ctypes_and_c_module/test_matrix.py
+++ experiments/python_ctypes_and_c_module/test_matrix.py
@@ -5,20 +5,10 @@ _rtgu = ctypes.CDLL("libRTGUcore.dylib")
 
 mat44 = ctypes.c_float * 16
 vec3  = ctypes.c_float * 3
-_rtgu.rtgu_mat44_rotationX.argtypes = [mat44, ctypes.c_float, ctypes.c_float]
+_rtgu.rtgu_mat44_rotation_x.argtypes = [mat44, ctypes.c_float, ctypes.c_float]
+_rtgu.rtgu_mat44_rotation_y.argtypes = [mat44, ctypes.c_float, ctypes.c_float]
+_rtgu.rtgu_mat44_rotation_z.argtypes = [mat44, ctypes.c_float, ctypes.c_float]
 _rtgu.rtgu_mul_mat44_vec3.argtypes = [mat44, vec3]
-
-mat = mat44()
-X = vec3(1, 0, 0)
-Y = vec3(0, 1, 0)
-Z = vec3(0, 0, 1)
-vec = vec3(*Y)
-
-_rtgu.rtgu_mat44_identity(mat)
-_rtgu.rtgu_mat44_rotationX(mat, 0.0, 1.0) # pi/2 rotation
-_rtgu.rtgu_mul_mat44_vec3(mat, vec)
-print [ f for f in mat ]
-print [ f for f in vec ]
 
 class Test(unittest.TestCase):
   
@@ -28,12 +18,30 @@ class Test(unittest.TestCase):
     X = vec3(1, 0, 0)
     Y = vec3(0, 1, 0)
     Z = vec3(0, 0, 1)
-    vec = vec3(*Y)
     
-    _rtgu.rtgu_mat44_rotationX(mat, 0.0, 1.0) # pi/2 rotation
+    # pi/2 rotation : rX * Y == Z
+    
+    vec = vec3(*Y)
+    _rtgu.rtgu_mat44_rotation_x(mat, 0.0, 1.0)
     _rtgu.rtgu_mul_mat44_vec3(mat, vec)
     
     self.assert_( _rtgu.rtgu_eq_vec3(vec, Z) )
+
+    # pi/2 rotation : rY * Z == X
+    
+    vec = vec3(*Z)
+    _rtgu.rtgu_mat44_rotation_y(mat, 0.0, 1.0)
+    _rtgu.rtgu_mul_mat44_vec3(mat, vec)
+    
+    self.assert_( _rtgu.rtgu_eq_vec3(vec, X) )
+
+    # pi/2 rotation : rZ * X == Y
+    
+    vec = vec3(*X)
+    _rtgu.rtgu_mat44_rotation_z(mat, 0.0, 1.0)
+    _rtgu.rtgu_mul_mat44_vec3(mat, vec)
+    
+    self.assert_( _rtgu.rtgu_eq_vec3(vec, Y) )
 
 
 if __name__ == "__main__":
