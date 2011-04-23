bd0ca2f296c05ce9776e2bec3474aa68416aa4dd
rotoglup@gmail.com <rotoglup@gmail.com@02a940b4-97ce-11de-a20b-5bf1f98922f7>
2011-04-23 21:24:57 +0000

    experiments: python ctypes

--- trunk/experiments/python_ctypes_and_c_module/rtgu_matrix.h
+++ trunk/experiments/python_ctypes_and_c_module/rtgu_matrix.h
@@ -5,7 +5,13 @@ typedef float rtgu_mat44[16];
 
 void rtgu_mat44_identity(rtgu_mat44 m);
 
-void rtgu_mat44_rotationX(rtgu_mat44 m, float cosx, float sinx);
+void rtgu_mat44_rotation_x(rtgu_mat44 m, float cosx, float sinx);
+void rtgu_mat44_rotation_y(rtgu_mat44 m, float cosx, float sinx);
+void rtgu_mat44_rotation_z(rtgu_mat44 m, float cosx, float sinx);
+
+void rtgu_mat44_scale(rtgu_mat44 m, float x, float y, float z);
+
+void rtgu_mat44_translation(rtgu_mat44 m, float x, float y, float z);
 
 void rtgu_mul_mat44_vec3(rtgu_mat44 m, float vec3[3]);
 
